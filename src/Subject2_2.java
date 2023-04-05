290359a42db3f3d63f551441ef94e89229cf1677
Chelsea Scott <chelseacat62@gmail.com>
2023-04-05 16:25:12 -0700

    created individual subject class for data creation

---
+++ src/Subject2_2.java
@@ -0,0 +1,19 @@
+
+/**
+ * Subject2_1 contains all the hard coded data for
+ * the subject Computational Thinking. There are 7
+ * questions.
+ **/
+
+public class Subject2_2 {
+
+    FlashCardMultipleChoice question1;
+    FlashCardTrueOrFalse question2;
+    FlashCardTrueOrFalse question3;
+
+    String[] answerChoicesM = new String[4];
+    int[] correctAnswerM = new int[4];
+    boolean answerChoiceTF;
+    boolean correctAnswerTF;
+
+}
