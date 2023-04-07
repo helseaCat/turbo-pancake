69bb6727e508528c122d19f2730c8a5fde13da2a
Chelsea Scott <chelseacat62@gmail.com>
2023-04-06 22:16:30 -0700

    added first iteration of CLI

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         BuildSubject current = new BuildSubject();
@@ -5,7 +7,22 @@ public class Main {
 
         twoOne.build2_1();
 
-        System.out.println("Hello world!");
-        System.out.println(twoOne.getCards());
+
+        // turn into method somewhere
+        FlashCardMultipleChoice flash;
+        ArrayList ans = twoOne.getCards();
+        flash = (FlashCardMultipleChoice) ans.get(0);
+
+
+        System.out.println("\n\n");
+        System.out.println(flash.getTheQuestion() + "\n");
+
+        // turn into print method somewhere
+        String[] arr = flash.getAnswerChoices();
+        for(int i = 0; i < 4; i++){
+            System.out.println((i+1) + ") " + arr[i]);
+        }
+
+        System.out.println("\n\n");
     }
 }
